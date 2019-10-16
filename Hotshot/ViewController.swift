ae0e5d74e2e722afd49c2e6fbe7cc826bc0ac7b1
Fırat <firat.ozkms@gmail.com>
2019-10-16 22:12:04 +0300

    Initial Commit

---
+++ Hotshot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hotshot
+//
+//  Created by Firot on 16.10.2019.
+//  Copyright © 2019 Firot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
