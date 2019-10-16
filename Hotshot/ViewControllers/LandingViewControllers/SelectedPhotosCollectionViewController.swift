161a4a55fbb6fac4b09d6060198e45ed05299d2e
Fırat <firat.ozkms@gmail.com>
2019-10-17 00:45:18 +0300

    ...

--- Hotshot/ViewControllers/LandingViewControllers/SelectedPhotosCollectionViewController.swift
+++ Hotshot/ViewControllers/LandingViewControllers/SelectedPhotosCollectionViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "PhotoCell"
 
 class SelectedPhotosCollectionViewController: UICollectionViewController {
 
@@ -20,41 +20,31 @@ class SelectedPhotosCollectionViewController: UICollectionViewController {
 
         // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
         // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 1000
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        cell.contentView.backgroundColor = .red
+        
         // Configure the cell
+        
+        cell.backgroundColor = .red
     
         return cell
     }
-
+    
     // MARK: UICollectionViewDelegate
 
     /*
