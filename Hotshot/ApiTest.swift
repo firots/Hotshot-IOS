507a6b0aef31e6d0897be5006f66817c4e9ae60f
Fırat <firat.ozkms@gmail.com>
2019-11-04 19:04:02 +0300

    ...

--- Hotshot/ApiTest.swift
+++ Hotshot/ApiTest.swift
@@ -12,10 +12,11 @@ import Alamofire
 class ApiTest {
     static func findSimilar(image: UIImage) {
         guard let imgData = image.jpegData(compressionQuality: 1.0) else { return }
-        
+        let imgName = "\(UUID.init().uuidString).jpeg"
         AF.upload(multipartFormData: { multipartFormData in
-            multipartFormData.append(imgData, withName: "image")
-        }, to: "http://3.17.157.138:5000/find_similar")
+            multipartFormData.append(imgData, withName: "image", fileName: imgName, mimeType: "image/jpeg")
+            }, to: "http://3.133.138.109:5000/find_similar"
+        )
             .responseJSON { response in
                 debugPrint(response)
             }
